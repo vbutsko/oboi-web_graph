18bc69bff76352227513adc82268d6bbb5104a34
Uladzislau Butsko <butko.wladislaw@gmail.com>
2017-10-07 16:07:01 +0300

    modified way of storing graph

--- src/main/java/butsko/oboi/App.java
+++ src/main/java/butsko/oboi/App.java
@@ -13,7 +13,7 @@ public class App
     public static void main( String[] args )
     {
         final GraphGenerator graphGenerator = new GraphGenerator(100, 10);
-        Map<Integer, Set<Integer>> graph = graphGenerator.getMNGraphes();
+        Map<Integer, Set<Integer>> graph = graphGenerator.getGraph();
         for(Map.Entry<Integer, Set<Integer>> vertex : graph.entrySet())
         {
             System.out.print(vertex.getKey() + " : ");
--- src/main/java/butsko/oboi/GraphGenerator.java
+++ src/main/java/butsko/oboi/GraphGenerator.java
@@ -5,47 +5,49 @@ import java.util.*;
 
 public class GraphGenerator
 {
-    private int n;
-    private int m;
     private int deg[];
     private int currentN;
+    private Map<Integer, Set<Integer>> graph;
 
 
     public GraphGenerator(int n, int m)
     {
-        this.n = n;
-        this.m = m;
         this.deg = new int[n * m];
         this.currentN = 0;
+        buildGraph(n, m);
     }
 
-    public Map<Integer, Set<Integer>> getMNGraphes()
+    public Map<Integer, Set<Integer>> getGraph()
+    {
+        return graph;
+    }
+
+    protected void buildGraph(int n, int m)
     {
         final Random random = new Random();
-        final Map<Integer, Set<Integer>> graphes = new HashMap<Integer, Set<Integer>>(n);
+        graph = new HashMap<>(n);
         for (int i = 0; i < m*n; i++)
         {
             currentN++;
             deg[i]++;
             int index = getVertexIndex(random.nextDouble());
             deg[index]++;
-            Set<Integer> vertexList = graphes.get(index);
+            Set<Integer> vertexList = graph.get(index);
             if (vertexList == null)
             {
                 vertexList = new HashSet<>(Arrays.asList(i));
-                graphes.put(index, vertexList);
+                graph.put(index, vertexList);
             }
             else
             {
                 vertexList.add(i);
             }
             final Set<Integer> newVertexList = new HashSet<>(Arrays.asList(index));
-            graphes.put(i, newVertexList);
+            graph.put(i, newVertexList);
         }
-        return graphes;
     }
-
-    private int getVertexIndex(double probability)
+    
+    protected int getVertexIndex(double probability)
     {
         int previousDegSum = deg[0];
         int index = 0;
